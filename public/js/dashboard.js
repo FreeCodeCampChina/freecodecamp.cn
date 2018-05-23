44c4cfc9bfb29589e313862aef2d0813e6980d70
chj-damon <chj_0507_dz@sina.com>
2018-05-23 10:00:02 +0800

    删除本次官网开发不需要的代码；

--- public/js/dashboard.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('app',[]);
-app.controller('dashboard',['$scope','$http', function($scope,$http){
-  $http.post('/dashboard').then(function(res){
-    var out = res.data;
-    $scope.items = out;
-  },function(err){
-    console.log(err);
-  })
-}])
